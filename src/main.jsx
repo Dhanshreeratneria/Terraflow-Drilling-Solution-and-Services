756bf1f03119e93d397bf7866262c47d85759b0b
Dhanshree <ratneriadhanshree@gmail.com>
2025-12-31 10:59:24 +0530

    Add Vercel Analytics and Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,15 @@ import ReactDOM from "react-dom/client";
 import { StrictMode } from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-    <AuthProvider>
+      <AuthProvider>
         <App />
-    </AuthProvider>
+        <SpeedInsights />
+      </AuthProvider>
     </BrowserRouter>
   </StrictMode>
-);  
+);
